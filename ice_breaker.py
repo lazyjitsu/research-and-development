9a929fb62d77e95b98951c25d32fdf791f043380
Marko <graphneenja@gmail.com>
2025-03-11 09:49:35 -0700

    added custom output parser for llm to prepare for frontend

--- ice_breaker.py
+++ ice_breaker.py
@@ -5,6 +5,7 @@ from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 from third_parties.twitter import scrape_user_tweets
+from output_parsers import summary_parser
 import os
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
@@ -19,24 +20,26 @@ def ice_breaker_with(name:str) -> str:
 
 
     summary_template = """
-        given the  information {information} about a person from Linkedin {information},
+        given the  information about a person from Linkedin {information},
         and twitter posts {twitter_posts} I want you to create:
         1. a short summary
         2. two interesting facts about them
 
-        Use both informatino from twitter and linkedin to create the summary
+        Use both information from twitter and linkedin to create the summary
+        \n{format_instructions}
     """
     summary_prompt_template = PromptTemplate(
-        input_variables=["information","twitter_posts"],template=summary_template
+        input_variables=["information","twitter_posts"],template=summary_template,
+        partial_variables={"format_instructions":summary_parser.get_format_instructions()},
     )
 
-    llm = ChatOpenAI(temperature=0,model='gpt-4o')
     # res = chain.invoke(input={"information":linkedin_data})
     
     llm = ChatOpenAI(temperature=0,model='gpt-4o')
 
     # chain = summary_prompt_template | llm 
-    chain = summary_prompt_template | llm | StrOutputParser()
+    # chain = summary_prompt_template | llm | StrOutputParser()
+    chain = summary_prompt_template | llm | summary_parser
     res = chain.invoke(input={"information":linkedin_data,"twitter_posts":tweets})
     print(res)
 
