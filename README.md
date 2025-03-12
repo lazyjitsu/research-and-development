3323c2565546e7c3fd9a0ff9d03c608d2f674c2d
Marko <graphneenja@gmail.com>
2025-03-12 16:04:57 -0700

    first comm again

--- README.md
+++ README.md
@@ -1,21 +1,9 @@
-# PreReqs
-`vagrant plugin install vagrant-hostmanager`
+# created a new environment by doing:
+pipenv shell
 
+# installed packages
+`pipenv install langchain langchain-openai black python-dotenv`
 
-# Research and Development
-- A review/recreation of past projects
-- identify the relevant technologies for stock trading
-
-Steps:
-  1. Obtain relevant/valid Vagrant file or run `vagrant init` to get the template
-  2. vagrant up | vagrant up $box e.g. --> vagrant up web01
-  3. vagrant ssh | vagrant ssh $box e.g. --> vagrant ssh web01
-  4. vagrant destroy | vagrant destroy $box --> vagrant destroy db01
-
-  5. vagrant status
-
-# check entire system for vms IMU
-# vagrant global-status  
-# vagrant global-status --prune # i believe cleans up cache. look this up
-# use the ids to do: vagrant destroy $id
+# to reformat code inside the dir
+`black .`
 
---
+++ main.py
@@ -0,0 +1,73 @@
+from dotenv import load_dotenv
+from langchain.agents import tool
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import render_text_description
+from langchain.agents.output_parsers.react_json_single_input import ReActJsonSingleInputOutputParser
+from langchain.schema import AgentAction, AgentFinish
+from typing import Union, List
+from langchain.tools import Tool, tool
+
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+@tool
+def get_text_length(text:str)->int:
+    """Returns the length of a text by characters"""
+    text  = text.strip("\n").strip('"')
+    return len(text)
+
+def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    raise ValueError(f"Tool wtih name {tool_name} not found")
+
+if __name__ == '__main__':
+   # print("hello React LangChain",get_text_length(text="cats"))
+   # since get_text_length is no longer a fcn but a structured tool, it can not be called normally. need to do it like below
+   # so the way you pass is using input=some dictionary
+    print("hello Reacts LangChain",get_text_length.invoke(input={"text":"catss"}))
+    tools = [get_text_length]
+
+    # this prompt will be sent to the LLM and this is going to generate the thought of the LLM and 
+    # help us select the correct tool
+    template = """
+        Answer the following questions as best you can. You have access to the following tools:
+
+        {tools}
+
+        Use the following format:
+
+        Question: the input question you must answer
+        Thought: you should always think about what to do
+        Action: the action to take, should be one of [{tool_names}]
+        Action Input: the input to the action
+        Observation: the result of the action
+        ... (this Thought/Action/Action Input/Observation can repeat N times)
+        Thought: I now know the final answer
+        Final Answer: the final answer to the original input question
+
+        Begin!
+
+        Question: {input}
+        Thought:
+        """
+    prompt = PromptTemplate.from_template(template=template).partial(tools=render_text_description(tools), tool_names = ", ".join([t.name for t in tools]))
+    # stop observation tells the LLM to stop generating text when it sees the word "Observation"
+    llm = ChatOpenAI(temperature = 0,stop =["\nObservation"])
+    # agent = prompt | llm
+    # lambda fcn that is receiving a dictionary and returning the value of the key "input"
+    agent = {"input": lambda x:x["input"]} | prompt | llm | ReActJsonSingleInputOutputParser()
+    
+    res = agent.invoke({"input":"What is the length of 'monster' in characters?'"})
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke({"input": "What is the length of the text 'wolf' in characters?"})
+    print(agent_step)
+
+
+if isinstance(agent_step, AgentAction):
+    tool_name = agent_step.tool
+    tool_to_use = find_tool_by_name(tools, tool_name)
+    tool_input = agent_step.tool_input
+    observation = tool_to_use.func(str(tool_input))
+    print(f"{observation=}")
