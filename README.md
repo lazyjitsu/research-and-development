67e1096cf1d97fdf9a98c37b651e550bd1410c33
Marko <graphneenja@gmail.com>
2025-03-14 13:18:44 -0700

    completed RAG 1

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ The same interface is a principle point.
 
 Pinecone is a bit viral as of this writing and is what people are using. We will be 
 using it in this branch.
+
+
+The basic workflow is:
+
+So overall, the basic behavior would be to take the original prompt of the user, embed it, and do some similarity search and find the relevant documents 
+Then simply plug it all to our RAG prompt and sending that to the LLM. And I know right now this looks like magic and we didn't take a look on the implementation of those chains, but trust me, this is what it's doing. And we'll be implementing in the following video the internals of those chains and to see how easy it is to implement it.
+
+Now it's important to note that if we didn't want to use the stuffing strategy, and let's say we wanted to summarize each document before sending it to the LLM, so we could have done that
+and we would simply need to put another chain that will do that instead of the combine_docs_chain that we plugged in.
+
+
+
--- ingestion.py
+++ ingestion.py
@@ -4,11 +4,13 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
+
 load_dotenv()
+
 if __name__ == '__main__':
     loader = TextLoader("D://mySource//Training//AI//Udemy//Documents//mediumblog1.txt")
     document = loader.load() # now its a langchain document
-
+    load_dotenv()
     # Now splilt into chunks. CharacterTextSplitter  splits the text into chunks of characters and can use regex
     # it can also use a diff leng fcn to calculate number of tokens. 1000 is a 'hueristic'/rule of thumb to keep
     # the chunks small enough to be processed by the model and fit in the context window. And should be big enough
@@ -22,6 +24,7 @@ if __name__ == '__main__':
     # obviously using OpenAI API to embed our documents
     embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
     print('Ingesting doco...')
-    PineconeVectorStore.from_documents(texts, embeddings,index_name=os.environ['INDEX_NAME'])
+    print('idx ',os.environ['INDEX_NAME'])
+    PineconeVectorStore.from_documents(texts, embeddings,index_name=os.environ.get("INDEX_NAME"))
     print("Finished ingestion")
     
