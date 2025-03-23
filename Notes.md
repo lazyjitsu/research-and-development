55cb5afcbfac9d54b0ffdfac4464640380b8f325
Marko <graphneenja@gmail.com>
2025-03-23 05:42:51 -0700

    added RAG utility; we now able to query pinecone vectorDB

--- Notes.md
+++ Notes.md
@@ -1,1 +1,9 @@
 $ wget -r -A.html -P langchain-docs https://langchain.readthedocs.io/en/latestwget -r -A.html -P langchain-docs https://langchain.readthedocs.io/en/latest^C
+
+
+### A note on combine_docs_chain
+
+Now, you might be asking yourself, hey, why do we call it combined docs chain?
+
+And the reason for that is because after we retrieve the relevant documents, we have a lot of options to do and perform optimizations
+and to perform actions on the relevant documents, maybe to summarize them, maybe to combine them into one huge string or to do some filtering afterwards. So that's where the combined docs chain is here for. So it gives us also the flexibility to plug in our own functionality for some post-processing after retrieval.
---
+++ backend/core.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+from langchain.chains.retrieval import create_retrieval_chain
+
+load_dotenv()
+
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+IDX_NAME="marcos-langchain-doc-idx"
+
+def run_llm(query:str):
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    docsearch = PineconeVectorStore(index_name=IDX_NAME, embedding=embeddings)
+    chat = ChatOpenAI(verbose=True, temperature=0)
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    # prompt + LLM + answer/relevant documents IMU
+    stuff_documents_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_prompt)
+    qa = create_retrieval_chain(
+        retriever=docsearch.as_retriever(), combine_docs_chain=stuff_documents_chain
+    )
+    result = qa.invoke(input={"input":query})
+    return result
+
+if __name__ == "__main__":
+    res = run_llm(query="What is LangChain Chain?")
+    print(res["answer"])
